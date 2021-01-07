aa7a9070393666832f4ee6ba855e600b7468566f
tom_glb <goodMorning_glb@atguigu.com>
2021-01-07 10:31:19 +0800

    分支添加

--- hello_git/src/main/java/com/hui/git/HelloGit.java
+++ hello_git/src/main/java/com/hui/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("第二次提交");
+        System.out.println("分支添加");
     }
 }
