c6457f53fb32a7da1faf48dc9a5c201fb29d8e3a
tom_glb <goodMorning_glb@atguigu.com>
2021-01-07 10:33:36 +0800

    模拟冲突 host_fix添加

--- hello_git/src/main/java/com/hui/git/HelloGit.java
+++ hello_git/src/main/java/com/hui/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("第二次提交");
         System.out.println("分支添加");
+        System.out.println("模拟冲突 host_fix");
     }
 }
