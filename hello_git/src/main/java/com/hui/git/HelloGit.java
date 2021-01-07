5f40100a3d609285f3a81f143d6cbc410e435774
tom_glb <goodMorning_glb@atguigu.com>
2021-01-07 10:26:33 +0800

    第一次提交

---
+++ hello_git/src/main/java/com/hui/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.hui.git;
+
+/**
+ * @author hui
+ * @create 2021/1/7-10:23
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
